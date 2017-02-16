0a1dafae84f3c3e72e32976c2c25b34566accbb0
Johannes Laier <JohannesLaier@gmail.com>
2017-02-16 17:07:45 +0100

    Anfangspopulation erstellt.

--- src/base/Population.java
+++ src/base/Population.java
@@ -4,4 +4,12 @@ import java.util.ArrayList;
 
 public class Population {
     private ArrayList<Tour> tours;
+
+    public Population() {
+        generateRandom();
+    }
+
+    public void generateRandom() {
+        // TODO Zufällige anfangs population erstellen.
+    }
 }
