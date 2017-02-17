a0b414022059a2c80457a0a9b950d2cd6b080e76
philippwi <philippwi95@gmail.com>
2017-02-17 09:09:43 +0100

    Bruteforce teilcode + test class

--- src/bruteforce/Bruteforce.java
+++ src/bruteforce/Bruteforce.java
@@ -1,6 +1,43 @@
 package bruteforce;
 
+
+import base.City;
+import data.InstanceReader;
+import data.TSPLIBReader;
+import random.MersenneTwisterFast;
+import java.util.ArrayList;
+
 public class Bruteforce {
+
+    private TSPLIBReader tspReader;
+    private InstanceReader instReader;
+    private int numberOfCities;
+    private ArrayList<City> cityList;
+
+    public Bruteforce(TSPLIBReader x, InstanceReader y, int z){
+        this.tspReader=x;
+        this.instReader=y;
+        this.numberOfCities=z;
+    }
+
+    private void getRandomCities(){
+        ArrayList<City> cityListAll=new ArrayList<City>();
+
+        MersenneTwisterFast mt = new MersenneTwisterFast();
+
+        cityListAll = tspReader.getCities();
+
+        for(int i=0; i<numberOfCities; i++) {
+            this.cityList.add(cityListAll.get(mt.nextInt(numberOfCities)));
+        }
+    }
+
+    public ... run(){
+        this.getRandomCities();
+
+
+    }
+
 
 
 }
