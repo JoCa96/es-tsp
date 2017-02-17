d8f11ee3be5c8adc6361481b70b9cc9d0f1500c4
felix <github@felix-schuetze.de>
2017-02-17 12:20:00 +0100

    Cycle umgebaut

--- src/test/crossover/HeuristicCrossoverTest.java
+++ src/test/crossover/HeuristicCrossoverTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 
 import base.City;
 import base.Tour;
+import crossover.CycleCrossover;
 import crossover.HeuristicCrossover;
 import test.AbstractTest;
 
@@ -16,19 +17,32 @@ public class HeuristicCrossoverTest extends AbstractTest{
 	@Test
 	public void testDoCrossover() {
 		System.out.println("Starte Test CycleCrossover");
-		Tour t1=Tour.generateRandom();
-		Tour t2=Tour.generateRandom();
+		Tour t1 = Tour.generateRandom();
+		Tour t2 = Tour.generateRandom();
 		System.out.println(t1);
 		System.out.println(t2);
-		Tour tk=new HeuristicCrossover().doCrossover(t1, t2);
+		Tour[] tk = new HeuristicCrossover().doCrossover(t1, t2);
 		System.out.println(tk);
 		HashMap<City, City> tmp = new HashMap<City, City>();
-		for(City c: tk.getCities())
-		{
+		for (City c : tk[0].getCities()) {
 			tmp.put(c, c);
 		}
-		if(tmp.size()!=tk.getSize())
+		if (tmp.size() != tk[0].getSize())
 			fail("Nicht alle Elemente vorhanden");
-	}
+		for (int i = 0; i < tk[0].getCities().size(); i++) {
+			if (!tk[0].getCity(i).equals(t1.getCity(i))) {
+				return;
+			}
+		}
+		for (City c : tk[1].getCities()) {
+			tmp.put(c, c);
+		}
+		if (tmp.size() != tk[1].getSize())
+			fail("Nicht alle Elemente vorhanden");
+		for (int i = 0; i < tk[1].getCities().size(); i++) {
+			if (!tk[1].getCity(i).equals(t1.getCity(i))) {
+				return;
+			}
+		}
 
 }
