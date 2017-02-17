c9b07e523fac3219dc919b1b37c71c4acd42b25a
felix <github@felix-schuetze.de>
2017-02-17 10:28:01 +0100

    Cycle geht

--- src/test/crossover/CycleCrossoverTest.java
+++ src/test/crossover/CycleCrossoverTest.java
@@ -12,25 +12,29 @@ import crossover.CycleCrossover;
 import main.Application;
 import test.AbstractTest;
 
-public class CycleCrossoverTest extends AbstractTest{
+public class CycleCrossoverTest extends AbstractTest {
 
 	@Test
 	public void testDoCrossover() {
 		System.out.println("Starte Test CycleCrossover");
 		new Application();
-		Tour t1=Tour.generateRandom();
-		Tour t2=Tour.generateRandom();
+		Tour t1 = Tour.generateRandom();
+		Tour t2 = Tour.generateRandom();
 		System.out.println(t1);
 		System.out.println(t2);
-		Tour tk=new CycleCrossover().doCrossover(t1, t2);
+		Tour tk = new CycleCrossover().doCrossover(t1, t2);
 		System.out.println(tk);
 		HashMap<City, City> tmp = new HashMap<City, City>();
-		for(City c: tk.getCities())
-		{
+		for (City c : tk.getCities()) {
 			tmp.put(c, c);
 		}
-		if(tmp.size()!=tk.getSize())
+		if (tmp.size() != tk.getSize())
 			fail("Nicht alle Elemente vorhanden");
+		for (int i = 0; i < tk.getCities().size(); i++) {
+			if (!tk.getCity(i).equals(t1.getCity(i))) {
+				return;
+			}
+		}
 	}
 
 }
