c9ee177ac2bcc04b90dc0fa38773989aa87d355a
Max <max_otti@web.de>
2017-02-17 13:39:07 +0100

    Fixed SubTourExchangeTest

--- src/test/crossover/SubTourExchangeCrossoverTest.java
+++ src/test/crossover/SubTourExchangeCrossoverTest.java
@@ -3,6 +3,7 @@ package test.crossover;
 import base.Tour;
 import org.junit.Test;
 import test.AbstractTest;
+import main.Configuration;
 
 /**
  * Created by Max on 17.02.2017.
@@ -14,22 +15,37 @@ public class SubTourExchangeCrossoverTest extends AbstractTest{
         Tour tour01 = Tour.generateRandom();
         Tour tour02 = Tour.generateRandom();
 
-        System.out.println(tour01);
-        System.out.println(tour02);
+        System.out.println("Parent 1:   "+ tour01);
+        System.out.println("Parent 2:   "+ tour02);
 
         Tour[] testresult = new SubTourExchangeCrossover().doCrossover(tour01, tour02);
 
-        System.out.println(testresult[0]);
-        System.out.println(testresult[1]);
+        System.out.println("Kind 1:     "+ testresult[0]);
+        System.out.println("Kind 2:     "+ testresult[1]);
 
-        if(testresult[0].getSize() != tour01.getSize() || testresult[1].getSize() != tour02.getSize())
+        Boolean equal = false;
+        int index = 0;
+
+        Tour child01 = testresult[0];
+        Tour child02 = testresult[1];
+
+        if(child01.getSize() != tour01.getSize() || child02.getSize() != tour02.getSize())
         {
             System.out.println("Zu wenig Zeichen!");
         }
-        else if (testresult[0].equals(tour01)||(testresult[1].equals(tour02)))
-        {
-            System.out.println("Klasse fehlerhaft");
+        else {
+            while (equal==true)
+            {
+                if((child01.getCity(index) != tour01.getCity(index))&& child02.getCity(index) != tour02.getCity(index))
+                    equal = false;
+                else equal = true;
+            }
+            if (equal == true)
+            {
+                System.out.println("Klasse fehlerhaft");
+            }
+            else System.out.println("Klasse!");
         }
-        else System.out.println("Klasse!");
+        System.out.println("Equal= "+ equal);
     }
 }
